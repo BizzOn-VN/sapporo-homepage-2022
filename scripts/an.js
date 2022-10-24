eede8a9a2913a81142cd12b44b0cea4638c0d46c
an <an0209vn@gmail.com>
2022-10-24 20:11:29 +0700

    udate html

--- scripts/an.js
+++ scripts/an.js
@@ -14,24 +14,53 @@
     // $( window ).resize(function() {
     //   owlWrapperWidth( $('.owl-wrapper') );
     // });
-$('.spr-slider .owl-carousel').owlCarousel({
-    items:1,
-    nav:false,
-    loop:true,
-    dots:true,
-    stagePadding:0,
-    margin:0,
-    URLhashListener:true,
-    autoplayHoverPause:true,
-    startPosition: 'URLHash',
-    autoplayTimeout:3000,
-    // autoplay:true,
-    autoplayHoverPause:true
+$(document).ready(function(){
+    var owl=$('.spr-slider .gglcs-slider').owlCarousel({
+        items:1,
+        nav:false,
+        loop:true,
+        dots:true,
+        stagePadding:0,
+        margin:0,
+        // animateIn: 'zoomIn',
+        URLhashListener:false,
+        startPosition: 'URLHash',
+        autoplayTimeout:3000,
+        autoplay:true,
+        autoplayHoverPause:true
+    });
+
+    owl.on('changed.owl.carousel', function(event) {
+       setTimeout(function(){
+
+
+         // var activeEls = $('.owl-item.active').eq(1); // .eq(1) to get the "middle image out of 3 actives"
+         var currentItem = $(".gglcs-slider .owl-item.active").find(".item").attr("data-hash");
+
+          window.location.hash = currentItem ;
+         // $(".spr-slider .owl-item").removeClass("target");
+         var data_hash=$(".gglcs-slider .owl-item.active").find(".item").attr("data-hash");
+         $(".spr-url-hash .item").removeClass("active");
+         $("." + data_hash).addClass("active");
+
+       },1);
+     });
+    // $(".spr-url-hash .item").click(function(){
+    //   $(".spr-url-hash .item").removeClass("active");
+    //   $(this).addClass("active");
+    // })
+
 });
+$(document).ready(function () {  
+  $(".spr-url-hash .sanh-an a")[0].click();
+}); 
+// function setCarouselCaption(el){
+  
+// }
 
 
-var owlCarousel = $('.owl-carousel').data('owl.carousel');
-owlCarousel.remove(1);
+// var owlCarousel = $('.owl-carousel').data('owl.carousel');
+// owlCarousel.remove(1);
 $('.slider-syncing-1').owlCarousel({
   loop:true,
   margin:0,
